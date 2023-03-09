9ca47fd6c1262018972d016d7bdea8d54ef84e09
Jakob Hansen <jakob.hansen@hotmail.no>
2023-03-09 11:47:25 +0100

    Redo tool architecture

--- thesis/figures/convert.sh
+++ thesis/figures/convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/find . -name *.drawio -exec rm -f {}.pdf \; -exec drawio --crop -x -o {}.pdf {} \;
+/usr/bin/find . -name \*.drawio -exec rm -f {}.pdf \; -exec drawio --crop -x -o {}.pdf {} \;
