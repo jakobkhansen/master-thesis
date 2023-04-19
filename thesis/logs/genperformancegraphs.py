f2003e7149d3ef812931ef4fd463a7df2ed2b35c
Jakob Hansen <jakob.hansen@hotmail.no>
2023-04-19 22:41:51 +0200

    Add performance benchmarks

--- thesis/logs/genperformancegraphs.py
+++ thesis/logs/genperformancegraphs.py
@@ -6,20 +6,50 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import pandas as pd
+import pathlib
 
-for file in os.scandir("performance/"):
+from pandas.core.frame import DataFrame
+
+for file in os.scandir("results/"):
     ccdetectfile = file.name
     if not ccdetectfile.endswith(".ccdetect"):
         continue
+    sacafile = ccdetectfile.split(".")[0] + ".saca"
+    print(sacafile)
     iclonesfile = ccdetectfile.split(".")[0] + ".iclones"
     chart_name = ccdetectfile.split(".")[0].split("_")[0] + " " + ccdetectfile.split(".")[0].split("_")[1]
-    print(iclonesfile)
-    ccdetect = pd.read_csv(f"performance/{ccdetectfile}", header=None)
-    iclones = pd.read_csv(f"performance/{iclonesfile}", header=None)
-    df = pd.DataFrame({"CCDetect": list(ccdetect.iloc[0]), "iClones": list(iclones.iloc[0])})
+
+    try:
+        saca = pd.read_csv(f"results/{sacafile}", header=None)
+    except:
+        print("SACA not foudn")
+        saca = None
+        pass
+
+    try:
+        ccdetect = pd.read_csv(f"results/{ccdetectfile}", header=None)
+    except:
+        ccdetect = None
+        pass
+
+    try:
+        iclones = pd.read_csv(f"results/{iclonesfile}", header=None)
+    except:
+        iclones = None
+        pass
+
+
+
+    df = pd.DataFrame({})
+    if (isinstance(saca, DataFrame)):
+        df["CCDetect SACA"] = list(saca.iloc[0])
+    if (isinstance(ccdetect, DataFrame)):
+        df["CCDetect incremental"] = list(ccdetect.iloc[0])
+    if (isinstance(iclones, DataFrame)):
+        df["iClones"] = list(iclones.iloc[0])
     print(df)
     ax = df.plot(kind="bar", rot=0, title=chart_name)
-    ax.set(xlabel="Insertions", ylabel="ms (log)", yscale="log")
+    ax.set(xlabel="Revision", ylabel="ms (log)", yscale="log")
 
     pdfname = ccdetectfile.split(".")[0].split("_")[0] + "_" + ccdetectfile.split(".")[0].split("_")[1]
     plt.savefig(f"../figures/performancegraphs/{pdfname}.pdf")
